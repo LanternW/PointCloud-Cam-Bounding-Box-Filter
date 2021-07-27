47f1caf91495ac6e36571caae4b87e1ef02934f9
Lantern <946229597@qq.com>
2021-07-27 10:57:24 +0800

    Add files via upload

---
+++ pc_measure/scripts/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist
+import time
+
+
+rospy.init_node("test_bounding_box",anonymous=True)
+
+pub = rospy.Publisher("/image_pixel", Twist,queue_size=10)
+
+t = Twist()
+t.linear.x = 10
+t.linear.y = 10
+t.angular.x = 1910
+t.angular.y = 1070
+
+while True:
+    pub.publish(t)
+    time.sleep(0.1)
+
